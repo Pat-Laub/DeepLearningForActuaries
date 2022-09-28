1df1daa6290fda342c6ce45eb1c9d93c363ae7b3
Patrick Laub <patrick.laub@gmail.com>
2022-09-28 11:13:47 +1000

    Add slides and Quarto source code for the 2022 version of the course

---
+++ Lecture-1-Artificial-Intelligence/user-input-loop.py
@@ -0,0 +1,11 @@
+while True:
+    userInput = input(">> What would you like to do? ")
+    print(userInput)
+    if userInput == "order cake":
+        print("Here's your cake! 🎂")
+
+    elif userInput == "order coffee":
+        print("Here's your coffee! ☕️")
+
+    elif userInput == "quit":
+        break
