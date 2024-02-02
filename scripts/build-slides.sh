fec7972ebd57822f4e4bcb634348c1fc74750f7f
Patrick Laub <patrick.laub@gmail.com>
2024-02-02 12:54:50 +1100

    Convert most weeks to use Keras v3 (can't yet for HF & TFP)

--- scripts/build-slides.sh
+++ scripts/build-slides.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 quarto render Lecture-1-Artificial-Intelligence/course-overview.qmd --no-execute-daemon
-(exit) && quarto render Lecture-1-Artificial-Intelligence/artificial-intelligence.qmd --no-execute-daemon
 (exit) && quarto render Lecture-1-Artificial-Intelligence/python.qmd --no-execute-daemon
-(exit) && quarto render Lecture-1-Artificial-Intelligence/chess-ai.qmd --no-execute-daemon
 (exit) && quarto render Lecture-2-Deep-Learning-Keras/deep-learning-keras.qmd --no-execute-daemon
-(exit) && quarto render Lecture-3-Mathematics-Of-Deep-Learning/mathematics-of-deep-learning.qmd --no-execute-daemon
-(exit) && quarto render Lecture-4-Network-Architectures-For-Tabular-Data/network-architectures-for-tabular-data.qmd --no-execute-daemon
-(exit) && quarto render Lecture-5-Recurrent-Neural-Networks-And-Time-Series/rnns-and-time-series.qmd --no-execute-daemon
-(exit) && quarto render Lecture-6-Computer-Vision/computer-vision.qmd --no-execute-daemon
-(exit) && quarto render Lecture-7-Natural-Language-Processing/natural-language-processing.qmd --no-execute-daemon
+(exit) && quarto render Lecture-3-Tabular-Data/categorical-variables.qmd --no-execute-daemon
+(exit) && quarto render Lecture-3-Tabular-Data/classification.qmd --no-execute-daemon
+(exit) && quarto render Lecture-4-Computer-Vision/computer-vision.qmd --no-execute-daemon
+(exit) && quarto render Lecture-5-Natural-Language-Processing/natural-language-processing.qmd --no-execute-daemon
+(exit) && quarto render Lecture-6-Uncertainty-Quantification/uncertainty-quantification.qmd --no-execute-daemon
+(exit) && quarto render Lecture-7-Recurrent-Neural-Networks-And-Time-Series/rnns-and-time-series.qmd --no-execute-daemon
 (exit) && quarto render Lecture-8-Generative-Networks/generative-networks.qmd --no-execute-daemon
-(exit) && quarto render Lecture-9-Advanced-Topics/advanced-topics.qmd --no-execute-daemon
+(exit) && quarto render Lecture-8-Generative-Networks/gans.qmd --no-execute-daemon
+(exit) && quarto render Lecture-9-Advanced-Topics/interpretability.qmd --no-execute-daemon
+(exit) && quarto render Lecture-9-Advanced-Topics/exam-revision.qmd --no-execute-daemon
 (exit) && quarto render index.qmd --no-execute-daemon
