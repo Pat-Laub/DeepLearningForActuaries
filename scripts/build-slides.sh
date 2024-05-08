d4902eedc9ed9f6962ab817a808655815b014ab6
Patrick Laub <patrick.laub@gmail.com>
2024-05-08 16:48:06 +1000

    Rerender with keras 3.3.3, quarto 1.4.554, and latest changes

--- scripts/build-slides.sh
+++ scripts/build-slides.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-quarto render Lecture-1-Artificial-Intelligence/course-overview.qmd --no-execute-daemon
+quarto render index.qmd --no-execute-daemon
+(exit) && quarto render Lecture-1-Artificial-Intelligence/course-overview.qmd --no-execute-daemon
 (exit) && quarto render Lecture-1-Artificial-Intelligence/artificial-intelligence.qmd --no-execute-daemon
 (exit) && quarto render Lecture-1-Artificial-Intelligence/python.qmd --no-execute-daemon
 (exit) && quarto render Lecture-2-Deep-Learning-Keras/deep-learning-keras.qmd --no-execute-daemon
@@ -15,4 +16,3 @@ quarto render Lecture-1-Artificial-Intelligence/course-overview.qmd --no-execute
 (exit) && quarto render Lecture-8-Generative-Networks/gans.qmd --no-execute-daemon
 (exit) && quarto render Lecture-9-Advanced-Topics/interpretability.qmd --no-execute-daemon
 (exit) && quarto render Lecture-9-Advanced-Topics/exam-revision.qmd --no-execute-daemon
-(exit) && quarto render index.qmd --no-execute-daemon
