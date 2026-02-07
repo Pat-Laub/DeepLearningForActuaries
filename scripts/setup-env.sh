29f110104da6e8ea876fecb97bff7f6ae73d3102
Patrick Laub <patrick.laub@gmail.com>
2026-02-07 17:13:14 +1100

    Simplify requirements.in, update to python 3.13 and update dependencies

--- scripts/setup-env.sh
+++
@@ -1,6 +0,0 @@
-# First install the Tensorflow/Keras & other packages.
-pip install -r requirements.in
-# Then install PyTorch.
-pip install -r torch_requirements.in
-# But lastly revert the CUDA library versions to what TensorFlow/Keras uses.
-pip install -r requirements.in
